7b81f24ec24f8dd3ff8d65b03d4fa30b94bee14b
Kibox99 <bozidar.kilibarda@gmail.com>
2022-01-17 09:27:23 +0100

    Push proj

---
+++ engine.py
@@ -0,0 +1,24 @@
+from logging import debug
+from flask import Flask, render_template
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'bsadaasgsdfwefwefwe asdada' #secretKey za cookies itd
+
+
+@app.route('/')#stavimo url endpointa 
+@app.route('/home') #znaci na ruti / i /home nam otvara home.html
+def home(): #kad odemo na url / sta god da je u home() ce raditi
+    return render_template('home.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True) 
