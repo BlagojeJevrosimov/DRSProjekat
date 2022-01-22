c4690e8274c2fe1081f33de06c77639d759e8879
Blagoje <blagojejevrosimov23@gmail.com>
2022-01-22 20:46:50 +0100

    Login i Register rute uradjene

--- engine.py
+++ engine.py
@@ -1,6 +1,8 @@
+from audioop import add
 from logging import debug
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify , request, flash, redirect, url_for
 from flaskext.mysql import MySQL
+from flask_login import login_user, login_required, logout_user, current_user
 
 import database_op
 from model.user import User,UserSchema
@@ -27,6 +29,8 @@ ma.init_app(app)
 
 
 @app.route('/')#stavimo url endpointa 
+def start():
+    return render_template('login.html')
 @app.route('/home') #znaci na ruti / i /home nam otvara home.html
 def home(): #kad odemo na url / sta god da je u home() ce raditi
 
@@ -46,19 +50,58 @@ def home(): #kad odemo na url / sta god da je u home() ce raditi
 
     return render_template('home.html', transactions = transactions, amount = amount)
 
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def register():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        firstName = request.form.get('firstName')
+        lastName = request.form.get('lastName')
+        country = request.form.get('country')
+        city = request.form.get('city')
+        address = request.form.get('address')
+        phoneNumber = request.form.get('phoneNumber')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        user = database_op.check_if_user_exists(email)
+        if user:
+            flash('Email already exists.', category='error')
+        elif len(firstName) < 2:
+            flash('First name required.', category='error')
+        elif len(lastName) < 2:
+            flash('Last name required.', category='error')
+        elif len(country) < 2:
+            flash('Country required.', category='error')
+        elif len(email) < 4:
+            flash('Email must be greater than 3 characters.', category='error')
+        elif ~email.__contains__('@'):
+            flash('Email must contain "@" characters.', category='error')
+        elif password1 != password2:
+            flash('Passwords don\'t match.', category='error')
+        elif len(password1) < 7:
+            flash('Password must be at least 7 characters.', category='error')
+        elif len(address) < 2:
+            flash('Address required.', category='error')
+        elif len(city) < 2:
+            flash('City required.', category='error')
+        elif len(phoneNumber) < 2:
+            flash('Phone number required.', category='error')
+        else:
+            database_op.register_user(email=email, firstName=firstName,lastName=lastName, password=password1,phone = phoneNumber,country=country,city=city,address= address)
+            login_user(User(email=email, firstName=firstName,lastName=lastName, password=password1,phone = phoneNumber,country=country,city=city,address= address), remember=True)
+            #flash('Account created!', category='success')
+            return redirect(url_for('templates.home'))
 
     
     #database_op.insert_transaction('djokssso@example.com', 2555, 'micoo', 'expense')
     #database_op.insert_user_amount('djoksssoss@example.com',255)
-    database_op.update_amount('djoksssoss@example.com',6000)
+    #database_op.update_amount('djoksssoss@example.com',6000)
 
-    user = User('bozidar@gmail.com','bozidar','kilibarda','55874',258746985,'srbija','novi sad','adresa')
+    #user = User('bozidar@gmail.com','bozidar','kilibarda','55874',258746985,'srbija','novi sad','adresa')
 
     return render_template('register.html')
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'] )
 def login():
 
     return render_template('login.html')
