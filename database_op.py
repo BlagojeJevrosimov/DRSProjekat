adeff481dc9e5991f38ad8a40e0ed37d93afa969
natasasc <scekicnnatasa@gmail.com>
2022-01-24 15:10:11 +0100

    Database update

--- database_op.py
+++ database_op.py
@@ -27,9 +27,52 @@ def validate_user(email):
 
 #TRANSACTION OPERATIONS
 
-def insert_transaction( sendingParty, amount, receivingParty, description):
+def insert_transaction(sendingParty, amount, receivingParty, description(sad)
     transaction = Transaction(sendingParty, amount, receivingParty, description)
     db.session.add(transaction)
+    db.session.commit()
+    transaction = Transaction.query.all()
+    return transaction[-1].transaction_id
+
+def successful_bank_transaction(transaction_id, amount,credit_number,user_account(sad)
+    transaction = Transaction.query.filter_by(transaction_id=transaction_id).first()
+    transaction.state = 'DONE'
+    #db.session.commit()
+    account = Amount.query.filter_by(email=user_account).first()
+    account.value = int(account.value) + int(amount)
+    #db.session.commit()
+    credit_card = Card.query.filter_by(card_num=credit_number).first()
+    credit_card.amount_dinar = int(credit_card.amount_dinar) - int(amount)
+    db.session.commit()
+
+def unsuccessful_transaction(transaction_id(sad)
+    transaction = Transaction.query.filter_by(transaction_id=transaction_id).first()
+    transaction.state = 'CANCELED'
+    db.session.commit()
+
+
+def successful_user_user_transaction(id,sender_email,receiver_email,amount(sad)
+    transaction = Transaction.query.filter_by(transaction_id=id).first()
+    transaction.state = 'DONE'
+
+    account = Amount.query.filter_by(email=sender_email).first()
+    account.value = int(account.value) - int(amount)
+
+    receiver_account = Amount.query.filter_by(email=receiver_email).first()
+    receiver_account.value = int(receiver_account.value) + int(amount)
+    db.session.commit()
+
+
+def successful_user_bank_transaction(id,sender_email,card_num,amount(sad)
+    transaction = Transaction.query.filter_by(transaction_id=id).first()
+    transaction.state = 'DONE'
+
+    account = Amount.query.filter_by(email=sender_email).first()
+    account.value = int(account.value) - int(amount)
+
+    credit_card = Card.query.filter_by(card_num=card_num).first()
+    credit_card.amount_dinar = int(credit_card.amount_dinar) + int(amount)
+
     db.session.commit()
 
 def get_transactions():
